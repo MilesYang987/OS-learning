82288d6426a19aa5f531ecfe0bafd004fdc177db
Miles <milesyang987@gmail.com>
2025-10-27 19:50:08 +0800

    Using C

---
+++ include/mynix/mynix.h
@@ -0,0 +1,11 @@
+#ifndef MYNIX
+#define MYNIX
+
+#define MYNIX_MAGIC 20251027
+
+
+
+void kernel_init();
+
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,8 @@
-void dummy_entry(){
-}
+#include <mynix/mynix.h>
+
+int magic = MYNIX_MAGIC;
+char message[] = "Hello Mynix!!!";
+char buf[1024];
 
 void main(){
     char* video_memory = (char*) 0xb8000;
