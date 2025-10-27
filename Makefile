9c759b224a202fddf46657ea411ed98ca139b463
Miles <milesyang987@gmail.com>
2025-10-27 16:41:26 +0800

    Entering Kernel

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+BUILD:=build
+SRC:=.
 C_SOURCES = $(wildcard kernel/*.c drivers/*.c)
 HEADERS = $(wildcard kernel/*.h drivers/*.h)
 
@@ -10,40 +12,53 @@ GDB = gdb
 
 CFLAGS = -g
 
+ENTRYPOINT:=0x10000
 
-os-image.bin: make/boot.bin make/loader.bin
-	dd if=/dev/zero of=$@ bs=512 count=10
-	dd if=make/boot.bin of=$@ bs=512 conv=notrunc count=1
-	dd if=make/loader.bin of=$@ bs=512 seek=1 count=4 conv=notrunc
+.PHONY: run build test clean
+
+os-image.bin: $(BUILD)/boot.bin $(BUILD)/loader.bin \
+	$(BUILD)/system.bin $(BUILD)/system.map
+	dd if=/dev/zero of=$@ bs=512 count=500
+	dd if=$(BUILD)/boot.bin of=$@ bs=512 count=1 conv=notrunc
+	dd if=$(BUILD)/loader.bin of=$@ bs=512 seek=1 count=4 conv=notrunc
+	dd if=$(BUILD)/system.bin of=$@ bs=512 seek=10 count=200 conv=notrunc
 
 
-.PHONY: run
 run: os-image.bin
 	qemu-system-i386 \
 	-drive format=raw,media=disk,file=$< \
 	-vga std
 
-.PHONY: build
 build: os-image.bin
 
-debug: os-image.bin make/boot.elf
+debug: os-image.bin $(BUILD)/boot.elf
 	qemu-system-i386 \
 	-drive format=raw,media=disk,file=$< \
 	-vga std &
-	${GDB} -ex "target remote localhost:1234" -ex "symbol-file make/boot.elf"
+	${GDB} -ex "target remote localhost:1234" -ex "symbol-file $(BUILD)/boot.elf"
 
-# Generic rules for wildcards
-# To make an object, always compile from its .c
+
 %.o: %.c ${HEADERS}
 	${CC} ${CFLAGS} -ffreestanding -c $< -o $@ -fno-pic -m32
 
-make/%.elf: boot/%.asm
-	nasm $< -f elf -o $@ ${INCLUDE}
+$(BUILD)/kernel/%.o: $(SRC)/kernel/%.asm
+	$(shell mkdir -p $(dir $@))
+	nasm -f elf32 $< -o $@ ${INCLUDE}
 
-make/%.bin: boot/%.asm
+$(BUILD)/kernel.bin: $(BUILD)/kernel/start.o
+	ld -m elf_i386 -static $^ -o $@ -Ttext $(ENTRYPOINT)
+
+$(BUILD)/%.bin: boot/%.asm
+	$(shell mkdir -p $(dir $@))
 	nasm $< -f bin -o $@ ${INCLUDE}
 
-.PHONY: clean
+$(BUILD)/system.bin: $(BUILD)/kernel.bin
+	objcopy -O binary $^ $@
+
+$(BUILD)/system.map: $(BUILD)/kernel.bin
+	nm $^ | sort > $@
+
+test: os-image.bin
+
 clean:
-	rm -rf make/*.bin *.o os-image.bin *.elf
-	rm -rf kernel/*.o drivers/*.o boot/*.o
+	rm -rf $(BUILD) *.bin
--- boot/loader.asm
+++ boot/loader.asm
@@ -28,7 +28,6 @@ loading:
     db "Loading OS...",10,13,0
 
 
-
 bios_print_hex:
     mov di, outstr+2
     mov ax, [print_data]
@@ -113,8 +112,84 @@ protected:
     mov esp, 0x10000
 
     mov byte [0xb8000], 'P'
+    
+    mov edi, 0x10000
+    mov ecx, 10
+    mov bl, 200
+    call read_disk
 
-    jmp $
+    jmp dword code_selector:0x10000
+
+    ud2
+
+
+read_disk:
+    mov dx, 0x1f2   ;number
+    mov al, bl
+    out dx, al
+
+    inc dx          ;0x1f3 sector addr low 8b
+    mov al, cl
+    out dx, al
+
+    inc dx          ;0x1f4 mid 8b
+    shr ecx, 8
+    mov al, cl
+    out dx, al
+
+    inc dx          ;0x1f5 high 8b
+    shr ecx, 8
+    mov al, cl
+    out dx, al
+
+    inc dx          ;0x1f6 0~3:sector addr 24~27
+    shr ecx, 8      ;4: 0 main 1 servant
+    and cl, 0b1111  ;6: 0 CHS 1 LBA
+
+    mov al, 0b1110_0000 ;5~7: fixed 1
+    or al, cl
+    out dx, al
+
+    inc dx          ;0x1f7 out
+    mov al, 0x20    ;0xec indentify disk
+    out dx, al      ;0x20 read
+                    ;0x30 write
+    xor ecx, ecx    
+    mov cl, bl      
+
+    .read:
+        push cx
+        call .waits
+        call .reads
+        pop cx
+        loop .read
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx           ;in(8b)
+            jmp $+2
+            jmp $+2
+            jmp $+2
+            and al, 0b1000_1000 ;0 ERR 3 DRQ 7 BSY
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0   ;16b IO data
+        mov cx, 256
+        .readw:
+            in ax, dx
+            jmp $+2
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
 
 
 
@@ -134,14 +209,14 @@ GDT_base:
 GDT_code:
     dw memory_limit & 0xffff        ;memory_limit low 15b
     dw memory_base & 0xffff         ;memory base 0~15b
-    db (memory_base >> 16) & 0xff   ;memory base 16~24b
+    db (memory_base >> 16) & 0xff   ;memory base 16~23b
     db 0b_1_00_1_1010               ;type(4) segment(1) DPL(2) present(1) type: x|c/e|r/w|a (x:1 data x:0 code a:accessed)
     db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf  ;memory limit 16~19b available(1) long mode(1) big(1) granularity(1)
     db (memory_base >> 24) & 0xff   ;memory base 24~31b
 GDT_data:
     dw memory_limit & 0xffff        ;memory_limit low 15b
     dw memory_base & 0xffff         ;memory base 0~15b
-    db (memory_base >> 16) & 0xff   ;memory base 16~24b
+    db (memory_base >> 16) & 0xff   ;memory base 16~23b
     db 0b_1_00_1_0010               ;type(4) segment(1) DPL(2) present(1) type: x|c/e|r/w|a (x:1 data x:0 code a:accessed)
     db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf  ;memory limit 16~19b available(1) long mode(1) big(1) granularity(1)
     db (memory_base >> 24) & 0xff   ;memory base 24~31b
